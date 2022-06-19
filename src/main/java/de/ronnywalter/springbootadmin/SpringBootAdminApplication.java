7fa0632e69b0e1e130b8e58e341cf95cf457c027
Ronny Walter <mail@ronnywalter.de>
2022-06-19 16:46:54 +0200

    .

--- src/main/java/de/ronnywalter/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/de/ronnywalter/springbootadmin/SpringBootAdminApplication.java
@@ -4,9 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 @EnableAdminServer
 public class SpringBootAdminApplication {
 
